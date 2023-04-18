c4cd8bfbb26af313dcafffc7ae4aa447a926698f
Naama Shiponi <naamas1234567@gmail.com>
2023-04-18 23:42:38 +0300

    add fun to Fraction

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -6,30 +6,72 @@ using namespace ariel;
 
 
 Fraction::Fraction(int num1,int num2):numerator(num1),denominator(num2){}
+Fraction::Fraction(float num1){
+    numerator=1;
+    denominator=1;
+}
+
+void Fraction::set(int numerator, int denominator){
+    this->numerator=numerator;
+    this->denominator=denominator;
+}
+
+
+Fraction Fraction::operator+(const Fraction& other) const{return other;}
+Fraction Fraction::operator-(const Fraction& other) const{return other;}
+Fraction Fraction::operator*(const Fraction& other) const{return other;}
+Fraction Fraction::operator/(const Fraction& other) const{return other;}
+
+
+bool Fraction::operator==(const Fraction& other) const {return false;}
+bool Fraction::operator!=(const Fraction& other) const {return false;}
+bool Fraction::operator<(const Fraction& other) const {return false;}
+bool Fraction::operator<=(const Fraction& other) const {return false;}
+bool Fraction::operator>(const Fraction& other) const {return false;}
+bool Fraction::operator>=(const Fraction& other) const {return false;}
+
+
+
+Fraction& Fraction::operator++(){return *this;}
+Fraction Fraction::operator++(int){return *this;}
+Fraction& Fraction::operator--(){return *this;}
+Fraction Fraction::operator--(int){return *this;}
+
+
+
 std::ostream& ariel::operator<<(std::ostream& os, const Fraction& f) {
     os <<" " << f.numerator << "/" << f.denominator;
     return os;
 }
 
-Fraction Fraction::operator+(const Fraction& other) const{return other;}
-Fraction Fraction::operator-(const Fraction& other) const{return other;}
-Fraction Fraction::operator/(const Fraction& other) const{return other;}
-Fraction Fraction::operator*(const Fraction& other) const{return other;}
-
-Fraction Fraction::operator++(int){return *this;}
-Fraction& Fraction::operator--(){return *this;}
-
-Fraction Fraction::operator+( double other) const{return *this;}
-Fraction Fraction::operator-( double other) const{return *this;}
-
-
-
-bool Fraction::operator>( double other) const{return false;}
-bool Fraction::operator>=(const Fraction& other) const{return false;}
-
-ariel::Fraction ariel::operator*(double d, const ariel::Fraction& f) {
-    return f;
+std::istream& operator>>(std::istream& is, Fraction& f) {
+    int numerator, denominator;
+    char c;
+    is >> numerator >> c >> denominator;
+    if (c != '/') {
+        is.setstate(std::ios::failbit);
+        return is;
+    }
+    f = Fraction(numerator, denominator);
+    return is;
 }
+
+bool ariel::operator==(const float& f, const Fraction& other) {return false;}
+bool ariel::operator!=(const float& f, const Fraction& other) {return false;}
+bool ariel::operator<(const float& f, const Fraction& other) {return false;}
+bool ariel::operator<=(const float& f, const Fraction& other) {return false;}
+bool ariel::operator>(const float& f, const Fraction& other) {return false;}
+bool ariel::operator>=(const float& f, const Fraction& other) {return false;}
+
+
+
+Fraction ariel::operator+(const float& f, const Fraction& other)  { return Fraction(0,0);}
+Fraction ariel::operator-(const float& f, const Fraction& other)  { return Fraction(0,0);}
+Fraction ariel::operator*(const float& f, const Fraction& other)  { return Fraction(0,0);}
+Fraction ariel::operator/(const float& f, const Fraction& other)  { return Fraction(0,0);}
+
+
+
 
 
 
--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -10,26 +10,50 @@ namespace ariel {
 
         public:
             Fraction(int num1,int num2);
-            friend std::ostream& operator<<(std::ostream& os, const Fraction& f);
+            Fraction(float num1);
+
+            void set(int numerator, int denominator);
+
             Fraction operator+(const Fraction& other) const;
             Fraction operator-(const Fraction& other) const;
-            Fraction operator/(const Fraction& other) const;
             Fraction operator*(const Fraction& other) const;
+            Fraction operator/(const Fraction& other) const;            
 
-            Fraction operator++(int); // C++
-            Fraction& operator--(); // --C
-            
-            Fraction operator+( double other) const;
-            Fraction operator-( double other) const;
 
-            bool operator>( double other) const;
+            bool operator==(const Fraction& other) const;
+            bool operator!=(const Fraction& other) const;
+            bool operator<(const Fraction& other) const;
+            bool operator<=(const Fraction& other) const;
+            bool operator>(const Fraction& other) const;
             bool operator>=(const Fraction& other) const;
 
+            
+            Fraction& operator++(); // ++C
+            Fraction operator++(int); // c ++
+            Fraction& operator--(); // --c
+            Fraction operator--(int); // c--
 
 
+            friend std::ostream& operator<<(std::ostream& os, const Fraction& f);
+            friend std::istream& operator>>(std::istream& is, Fraction& f);
+
+
+            friend bool operator==(const float& f, const Fraction& other);
+            friend bool operator!=(const float& f, const Fraction& other);
+            friend bool operator<(const float& f, const Fraction& other);
+            friend bool operator<=(const float& f, const Fraction& other);
+            friend bool operator>(const float& f, const Fraction& other);
+            friend bool operator>=(const float& f, const Fraction& other);
+
+
+
+            friend Fraction operator+(const float& f, const Fraction& other);
+            friend Fraction operator-(const float& f, const Fraction& other);
+            friend Fraction operator*(const float& f, const Fraction& other);
+            friend Fraction operator/(const float& f, const Fraction& other);
     };
 
-Fraction operator*(double d, const Fraction& f);
+
 };
 
 
